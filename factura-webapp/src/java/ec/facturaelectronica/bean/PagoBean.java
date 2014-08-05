6bd35eb89b827e524c5729cda99535de9c48ea68
Armando <Armando@Jupiter>
2014-08-05 00:08:06 -0500

    Se añaden nuevas funcionalidades

--- factura-webapp/src/java/ec/facturaelectronica/bean/PagoBean.java
+++ factura-webapp/src/java/ec/facturaelectronica/bean/PagoBean.java
@@ -16,6 +16,8 @@ import ec.facturaelectronica.model.TipoComprobante;
 import ec.facturaelectronica.service.GeneraPagoService;
 import ec.facturaelectronica.util.RecursosServices;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
@@ -27,6 +29,7 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import org.primefaces.context.RequestContext;
@@ -45,6 +48,9 @@ public class PagoBean extends RecursosServices implements Serializable {
 
     @EJB
     private GeneraPagoService pagoService;
+    
+    @ManagedProperty(value = "#{loginAccessBean}")
+    private LoginAccessBean loginAccessBean;
 
     private Empresa empresaSelected;
     private List<Empresa> empresas;
@@ -113,15 +119,31 @@ public class PagoBean extends RecursosServices implements Serializable {
     public void generar() throws ServicesException {
         FacesMessage msg;
         pago = pagoService.generarPago(empresaSelected, fechaGeneracion);
+        pago.setExcedenteComprobantesPago(getExedente());
+        pago.setCostoFactPlanPago(empresaSelected.getPlan().getValorFacturaPlan());
+        pago.setCostoPlanPago(empresaSelected.getPlan().getCostoPlan());
+        pago.setUsuario(loginAccessBean.getUsuarioLogin());
+        pago.setFechaRegistroPago(new Date());
+        pago.setNumeroComprobantesPago(totalComprobantes);
+        pago.setPlan(empresaSelected.getPlan());
+        pago.setTotalPago(new BigDecimal(getValorAPagar())); 
         if (pagoService.obtenerPagosPorEmpresaPorMes(empresaSelected, pago.getMesPago()).isEmpty()) {
             getComprobanteList();
             setVisible(true);
         }else{
 //            Arreglar recurso
-            msg = new FacesMessage(FacesMessage.SEVERITY_WARN, recurso.getString("empresa.header"), "El pago ha sido efectuadocon anterioridad");
+            msg = new FacesMessage(FacesMessage.SEVERITY_WARN, recurso.getString("empresa.header"), "El pago ha sido efectuado con anterioridad");
             FacesContext.getCurrentInstance().addMessage(null, msg);
             RequestContext.getCurrentInstance().update("form:growl");           
             setVisible(false);   
+        }
+    }
+    
+    public void guardarPago(){
+        try {
+            pagoService.guardarPago(pago);
+        } catch (ServicesException ex) {
+            Logger.getLogger(PagoBean.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
@@ -283,6 +305,10 @@ public class PagoBean extends RecursosServices implements Serializable {
 
     public void setTotalComprobantes(int totalComprobantes) {
         this.totalComprobantes = totalComprobantes;
+    }
+
+    public void setLoginAccessBean(LoginAccessBean loginAccessBean) {
+        this.loginAccessBean = loginAccessBean;
     }
 
 }
